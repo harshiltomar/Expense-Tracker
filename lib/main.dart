8aa045ad29c123a9396fa13d1b4484a583992406
harshiltomar <harshiltomar20@gmail.com>
2022-12-18 14:22:07 +0530

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:expensetracker/models/transactions.dart';
+import 'package:expensetracker/widgets.dart/users_transactions.dart';
 import 'package:flutter/material.dart';
-import 'models/transactions.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter App',
       home: MyHomePage(),
     );
@@ -18,9 +17,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
-  // late String titleInput;
-  // late String amountInput;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
@@ -40,81 +36,7 @@ class MyHomePage extends StatelessWidget {
               child:
                   Card(color: Colors.blue, elevation: 5, child: Text('CHART!')),
             ),
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      // onChanged: (val) {
-                      //   titleInput = val;
-                      // },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      // onChanged: (val) {
-                      //   amountInput = val;
-                      // },
-                    ),
-                    TextButton(
-                      style:
-                          TextButton.styleFrom(foregroundColor: Colors.purple),
-                      child: Text('Add Transaction'),
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ));
-              }).toList(),
-            )
+            UserTransactions()
           ],
         ));
   }
